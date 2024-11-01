48e12df871041cd0448ad1873955514cd7ac53c4
Ondra Machacek <machacek.ondra@gmail.com>
2024-11-01 10:56:09 +0100

    Nove

---
+++ stastnyramar_files/lang.cz.1434.js
@@ -0,0 +1,14 @@
+// Copyright 2006 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+window.translations=["Free","Mini","Standard","Profi","Limited","Business","Blog","Produkty","Zatím nemáte žádné články. Začněte kliknutím na \"Přidat článek\".","V e-shopu zatím nemáte žádný produkt. Klikněte na \"Přidat produkt\".","Zatím nemáte žádné články. Začněte přidáním stránky \"Blog\".","Tato kategorie je prázdná. Přidejte do ní produkty, nebo zvolte jinou kategorii.","Fotogalerie","Tato fotogalerie neobsahuje žádné obrázky.","Nezobrazovat","Skladem","Vyprodáno","Dostupné do 3 dnů","Dostupné do 7 dnů","Dostupné do 14 dnů","Dostupné do měsíce","Dostupné za více než měsíc"];
